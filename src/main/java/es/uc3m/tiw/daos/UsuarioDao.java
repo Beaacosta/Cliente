1708360f1d5d7345832ff9aae4b8ef93aa65bad0
Beaacosta <100304636@alumnos.uc3m.es>
2016-12-01 16:09:24 +0000

    creado usuario

--- src/main/java/es/uc3m/tiw/daos/UsuarioDao.java
+++ src/main/java/es/uc3m/tiw/daos/UsuarioDao.java
@@ -1,10 +1,15 @@
 
 package es.uc3m.tiw.daos;
 
+import java.sql.SQLException;
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import es.uc3m.tiw.dominios.Usuario;
 
 public interface UsuarioDao extends CrudRepository<Usuario, Long> {
+
+	//Mejor usar JPA que crudRepository (investigar)
 
 }
--- src/main/java/es/uc3m/tiw/dominios/Usuario.java
+++ src/main/java/es/uc3m/tiw/dominios/Usuario.java
@@ -1,57 +1,96 @@
 package es.uc3m.tiw.dominios;
+import static javax.persistence.GenerationType.AUTO;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.OneToOne;
+import static javax.persistence.CascadeType.ALL;
 
 @Entity
-public class Usuario {
+@Table(name="USUARIO")
+public class Usuario implements Serializable {
 	@Id
-	@GeneratedValue
-	private Long id;
-	private String nombre;
-	private int edad;
+	@GeneratedValue(strategy = AUTO)
+	private int id;
+	@Column(length = 30)
+	private String apellidos;
+	@Column(nullable = false)
+	private String ciudad;
+	@Column(nullable = false, length = 30, unique = true)
+	private String mail;
+	@Column(nullable = false, length = 15)
+	private String nombre;	
+
+	@Column(nullable = false)
+	private String password;
 	
-	/*
-	@OneToMany(cascade=CascadeType.ALL)
-	@JoinColumn(name="usuarioId")
-	private Set<Producto> producto;
-	*/
-	
-	public Usuario(){
+	/*@OneToOne(cascade = ALL)
+	private Direccion direccion;*/
 		
+	public Usuario() {
+		// TODO Auto-generated constructor stub
 	}
-
-	public Usuario(Long id, String nombre, int edad) {
+	public Usuario(String nombre, String apellidos, String ciudad,
+			String password) {
 		super();
-		this.id = id;
 		this.nombre = nombre;
-		this.edad = edad;
+		this.apellidos = apellidos;
+		this.ciudad = ciudad;
+		this.password = password;
 	}
-
-	public Long getId() {
-		return id;
+	public Usuario(String mail, String password) {
+		super();
+		this.mail = mail;
+		this.password = password;
 	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	public int getEdad() {
-		return edad;
-	}
-
-	public void setEdad(int edad) {
-		this.edad = edad;
+	
+	public String getApellidos() {
+		return apellidos;
 	}
 	
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
 	
+	public String getCiudad() {
+		return ciudad;
+	}
+	
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getMail() {
+	return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+		
 }
