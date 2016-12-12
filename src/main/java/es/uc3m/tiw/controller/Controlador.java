4f6d63f0cf0e41a615ad7916bf2eb9e55593943d
Beaacosta <100304636@alumnos.uc3m.es>
2016-12-12 21:01:57 +0000

    controller hecho

--- src/main/java/es/uc3m/tiw/controller/Controlador.java
+++ src/main/java/es/uc3m/tiw/controller/Controlador.java
@@ -1,5 +1,7 @@
 package es.uc3m.tiw.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,13 +24,45 @@ public class Controlador {
 	private Usuario usuario;
 	private UsuarioDao dao;
 	
+	//Buscar un usuario en la BBDD por id
 	@RequestMapping(value = "/buscar_id", method = RequestMethod.GET)
-	public Usuario usuarioGet(@RequestParam(value = "id", required = true) int id){
+	public Usuario buscarId(@RequestParam(value = "id", required = true) int id){
 		usuario = new Usuario();
-		//metodo dao.buscar...
+		usuario = dao.findById(id);
 		return usuario;
+	}
+	
+	//Buscar un usuario en la BBDD por email
+	@RequestMapping(value = "/buscar_mail", method = RequestMethod.GET)
+	public Usuario buscarMail(@RequestParam(value = "mail", required = true) String mail){
+		usuario = new Usuario();
+		usuario = dao.findByMail(mail);
+		return usuario;
+	}
 		
+	//Listar todos los usuarios
+	@RequestMapping(value = "/listar_usuarios", method = RequestMethod.GET)
+	public List<Usuario> listarUsuarios(){
+		List<Usuario> usuarios = null;
+		usuarios = dao.findAll();
+		return usuarios;
 	}
 
-
+	//Eliminar un usuario
+	@RequestMapping(value = "/eliminar_usuario", method = RequestMethod.DELETE)
+	public void eliminarUsuario(@RequestParam(value = "id", required = true) int id){
+		usuario = dao.findById(id);
+		dao.delete(usuario);
+	}
+	
+	//Añadir un usuario
+	@RequestMapping(value = "/anyadir_usuario", method = RequestMethod.PUT)
+	public void anyadirUsuario(Usuario usuario){
+		dao.save(usuario);
+	}
+	//Modificar un usuario
+	@RequestMapping(value = "/modificar_usuario", method = RequestMethod.PUT)
+	public void modificarProducto(Usuario usuario){
+		dao.save(usuario);
+	}
 }
--- src/main/java/es/uc3m/tiw/daos/UsuarioDao.java
+++ src/main/java/es/uc3m/tiw/daos/UsuarioDao.java
@@ -1,15 +1,14 @@
 
 package es.uc3m.tiw.daos;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+import java.util.List;
 import es.uc3m.tiw.dominios.Usuario;
 
 public interface UsuarioDao extends JpaRepository<Usuario, Long>{
 
-	//Mejor usar JPA que crudRepository (investigar)
-
+	List<Usuario> findAll();
+	Usuario findById(int id);
+	Usuario findByMail(String mail);
 }
